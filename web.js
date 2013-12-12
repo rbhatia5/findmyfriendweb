6ed01bb286d3d03185bb90d3f5537ea0a5185ee2
Rahul Bhatia <rahul.bhatia.k@gmail.com>
2013-12-12 07:13:17 -0600

    added db management and pull data function for testing on heroku

--- web.js
+++ web.js
@@ -3,6 +3,19 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function(err, db) {
+	db.collection('mydocs',function(er, collection) {
+		collection.insert({'mykey':'myvalue'}, {safe: true}, function(er,rs){
+			
+		});
+	});
+});
+
+
 app.use(logfmt.requestLogger());
 
 app.configure(function(){
@@ -15,9 +28,14 @@ app.get('/1/dropdata', function(req, res) {
 	
 });
 
+app.get('/1/pulldata' , function(req, res) {
+	console.log(req.body);
+	res.send("grabbing data");
+});
+
 app.get('/', function(req,res) {
 	res.send("Hello World!");
-});
+});h
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
